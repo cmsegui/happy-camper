6114dd19cad3374b1ac0c97b5e82d843654f5f95
cydmarie <cydmarie.dev@gmail.com>
2017-11-16 05:30:25 +0000

    added new and post routes for camp sites with test data before adding a database

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
+app.use(bodyParser.urlencoded ({extended: true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+        {name: "Victoria Bryant", image: "https://static.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg"}, 
+        {name: "Unicoi", image: "https://static.pexels.com/photos/176381/pexels-photo-176381.jpeg"},
+        {name: "Tallulah Gorge", image: "https://static.pexels.com/photos/558454/pexels-photo-558454.jpeg"}
+];
+        
+        
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-    var campgrounds = [
-        {name: "Victoria Bryant", image: "https://static.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg"}, 
-        {name: "Unicoi", image: "https://static.pexels.com/photos/176381/pexels-photo-176381.jpeg"},
-        {name: "Tallulah Gorge", image: "https://static.pexels.com/photos/558454/pexels-photo-558454.jpeg"}
-        ]
-        
-        res.render("campgrounds", { campgrounds: campgrounds });
+    res.render("campgrounds", { campgrounds: campgrounds });
+});
+
+app.post("/campgrounds", function(req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = { name: name, image: image };
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+});
+
+
+app.get("/campgrounds/new", function(req, res) {
+   res.render("new"); 
 });
 
 
