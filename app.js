95eed73b6821d6432dc159949a637eb9b70c81b9
cydmarie <cydmarie.dev@gmail.com>
2017-11-17 06:05:52 +0000

    added mongoose to project and began working with database. found problem with new campground image now showing and corrected this

--- app.js
+++ app.js
@@ -1,32 +1,56 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
 
+mongoose.connect("mongodb://localhost/happy-camper", {useMongoClient: true});
 app.use(bodyParser.urlencoded ({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-        {name: "Victoria Bryant", image: "https://static.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg"}, 
-        {name: "Unicoi", image: "https://static.pexels.com/photos/176381/pexels-photo-176381.jpeg"},
-        {name: "Tallulah Gorge", image: "https://static.pexels.com/photos/558454/pexels-photo-558454.jpeg"}
-];
-        
-        
+var campgroundSchema = new mongoose.Schema({
+    name: String, 
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({name: "Unicoi", 
+//                   image: "https://static.pexels.com/photos/176381/pexels-photo-176381.jpeg"
+// }, function(err, campground) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("New campground");
+//         console.log(campground);
+//     }
+// });
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-    res.render("campgrounds", { campgrounds: campgrounds });
+    Campground.find({}, function(err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", { campgrounds: allCampgrounds });
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = { name: name, image: image };
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    })
 });
 
 
