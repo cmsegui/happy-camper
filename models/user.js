912243d59f864e74f8aba8a562f6d61793705274
cydmarie <cydmarie.dev@gmail.com>
2017-11-24 19:49:16 +0000

    added passport mongoose plugin to user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
     username: String,
@@ -6,7 +7,7 @@ var UserSchema = new mongoose.Schema({
 });
 
 
-
+UserSchema.plugin(passportLocalMongoose);
 
 
 
