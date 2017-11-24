42dec5a71d44f4820aa0eedfed6f46bf999ab4ca
cydmarie <cydmarie.dev@gmail.com>
2017-11-24 04:39:30 +0000

    added the comment post route and made sure it worked.  added button to add new comments.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var bodyParser = require("body-parser");
 var app = express();
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
+var Comment = require("./models/comments");
 var seedDB = require("./seeds");
 mongoose.Promise = global.Promise;
 
@@ -72,6 +73,28 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
             res.render("comments/new", { campground: campground });
         }
     })
+});
+
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+    Campground.findById(req.params.id, function(err, campground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        }
+        else {
+            Comment.create(req.body.comment, function(err, comment) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            })
+        }
+    });
 });
 
 
