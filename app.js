4727e9741ddce61baf82592d444d16e42f260e1b
cydmarie <cydmarie.dev@gmail.com>
2017-11-25 05:14:57 +0000

    functionality to navbar, shows username of logged in, links appear/disappear depending on whether signed in or not

--- app.js
+++ app.js
@@ -31,7 +31,10 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
 
 //===================CAMPGROUND ROUTES======================
 app.get("/", function(req, res) {
