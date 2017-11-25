25e8c3d5cd580fbfe9a63d19ea733d2f487d4cf8
cydmarie <cydmarie.dev@gmail.com>
2017-11-25 04:06:48 +0000

    added login routes with user authentication and created login form

--- app.js
+++ app.js
@@ -139,6 +139,25 @@ app.post("/register", function(req, res) {
 
 
 
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+}), function(req, res) {
+
+});
+
+
+
+
+
+
+
+
+
 
 
 
