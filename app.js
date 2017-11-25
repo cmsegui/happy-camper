43fd3bf2000ba6539e1efcc5754803b31069bfb1
cydmarie <cydmarie.dev@gmail.com>
2017-11-25 04:43:11 +0000

    user needs to be logged in to leave comment - added this functionality

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.get("/campgrounds/:id", function(req, res) {
 
 //====================COMMENT ROUTES================================
 
-app.get("/campgrounds/:id/comments/new", function(req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
     Campground.findById(req.params.id, function(err, campground) {
         if (err) {
             console.log(err);
@@ -96,7 +96,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
 });
 
 
-app.post("/campgrounds/:id/comments", function(req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
     Campground.findById(req.params.id, function(err, campground) {
         if (err) {
             console.log(err);
@@ -154,13 +154,18 @@ app.post("/login", passport.authenticate("local", {
 
 app.get("/logout", function(req, res) {
     req.logout();
-    req.redirect("/campgrounds");
+    res.redirect("/campgrounds");
 })
 
 
 
 
-
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 
 
