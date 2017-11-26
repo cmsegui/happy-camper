86e7c1bcf5c13bc19d5affc4da823ecf6f6cbf7b
cydmarie <cydmarie.dev@gmail.com>
2017-11-26 23:49:12 +0000

    added delete route and form/button

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -73,6 +73,20 @@ router.put("/:id", function(req, res) {
 })
 
 
+router.delete("/:id", function(req, res) {
+    Campground.findByIdAndRemove(req.params.id, function(err) {
+        if (err) {
+            res.redirect("/campgrounds");
+        }
+        else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+
+
+
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
