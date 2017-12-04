fb705147de6842cbc7a3d85f5e07e152c1113018
cydmarie <cydmarie.dev@gmail.com>
2017-12-04 18:25:56 +0000

    made the price feature dynamic and began working with google maps api

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var router = express.Router();
 var Campground = require("../models/campground");
-var middleware = require("../middleware")
+var middleware = require("../middleware");
+var geocoder = require('geocoder');
 
 router.get("/", function(req, res) {
     Campground.find({}, function(err, allCampgrounds) {
@@ -16,13 +17,14 @@ router.get("/", function(req, res) {
 
 router.post("/", middleware.isLoggedIn, function(req, res) {
     var name = req.body.name;
+    var price = req.body.price;
     var image = req.body.image;
     var desc = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     };
-    var newCampground = { name: name, image: image, description: desc, author: author };
+    var newCampground = { name: name, price: price, image: image, description: desc, author: author };
     Campground.create(newCampground, function(err, newlyCreated) {
         if (err) {
             console.log(err);
